2828537ce5fbfbfa4a809cb5256f3c59715f4dd6
sergeyk321 <sergeyk321@yandex.ru>
2022-12-14 21:53:42 +0300

    task 5

--- src/Tasks/Task5.java
+++ src/Tasks/Task5.java
@@ -9,6 +9,9 @@ import javax.xml.bind.DatatypeConverter;
 
 public class Task5 {
     public static void main(String[] args) throws Exception {
+        System.out.println("Task 1/10");
+        System.out.println(encrypt("Hello"));
+        System.out.println(decrypt(new int [] {72, 33, -73, 84, -12, -3, 13, -13, -68,}));
         System.out.println("Task 2/10");
         System.out.println(canMove("Rook", "A8", "H8"));
         System.out.println(canMove("Bishop", "A7", "G1"));
@@ -23,12 +26,41 @@ public class Task5 {
         System.out.println("Task 4/10");
         System.out.println(sumDigProd(16, 28));
         System.out.println(sumDigProd(1, 2, 3, 4, 5, 6));
+        System.out.println("Task 5/10");
+        System.out.println(sameVowelGroup(new String [] {"toe", "ocelot", "maniac"}));
+        System.out.println(sameVowelGroup(new String [] {"hoops", "chuff", "bot", "bottom"}));
+        System.out.println("Task 6/10");
+        System.out.println(validateCard(123123123123123L));
         System.out.println("Task 7/10");
-        System.out.println(numToEng(532));
+        System.out.println(numToEng(35));
         System.out.println(numToRus(832));
         System.out.println("Task 8/10");
         System.out.println(getSha256Hash("Hello"));
         System.out.println(getSha256Hash("password123"));
+        System.out.println("Task 9/10");
+        System.out.println(correctTitle("jOn snOw, kiNg of The north"));
+        System.out.println("Task 10/10");
+        System.out.println(haxLattice(19));
+    }
+    // 1/10
+    public static String encrypt(String s) {
+        int [] arr1 = new int[s.length()];
+        int [] arr2 = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) 
+            arr1[i] = (int) s.charAt(i);
+        arr2[0] = arr1[0];
+        for (int i = 1; i < arr1.length; i++) 
+            arr2[i] = arr1[i] - arr1[i - 1];
+        return Arrays.toString(arr2);
+    }
+    public static String decrypt(int [] array) {
+        String s = new String();
+        s += (char) array[0];
+        for (int i = 1; i < array.length; i++) {
+            array[i] = array[i - 1] + array[i];
+            s += (char) (array[i]);
+        }
+        return s;
     }
     // 2/10
     public static boolean canMove(String s, String n, String m) {
@@ -43,7 +75,7 @@ public class Task5 {
         if (s.contains("Bishop"))  //Слон
             return Math.abs((a2 - b2) / (l1 - l2)) == 1; //по диагонали, тогда х и у смещаются поровну => -1 или 1
         if (s.contains("Pawn")) //Пешка
-            return ((l1 == l2) && (Math.abs(a2 - b2) == 1)); //координаты х равны, по у = 1 шаг
+            return ((l1 == l2) && (Math.abs(a2 - b2) <= 2)); //координаты х равны, по у = 1 или 2
         if (s.contains("Knight"))  //Конь
             return ((Math.abs(l2 - l1) == 1 && Math.abs(a2 - b2) == 2 // шаг по х = 1, по у = 2
             ||       Math.abs(l2 - l1) == 2 && Math.abs(a2 - b2) == 1)); // шаг по х = 2, по у = 1
@@ -51,7 +83,7 @@ public class Task5 {
             return (Math.abs((a2 - b2) / (l1 - l2)) == 1 //для слона
             ||  ((Math.abs(l1 - l2) == 1 && Math.abs(b2 - a2) == 1))); //для короля
         if (s.contains("King"))  //Король
-            return ((Math.abs(l1 - l2) == 1 && Math.abs(b2 - a2) == 1)); //есть смещение = 1 по х и по у
+            return ((Math.abs(l1 - l2) <= 1 && Math.abs(b2 - a2) <= 1)); //есть смещение <= 1 по х и по у
         return false;
     }
     // 3/10
@@ -89,13 +121,71 @@ public class Task5 {
         return n;
     }
     // 5/10
-    // public static ArrayList<String> sameVowelGroup(String [] arr) {
-    //     String letters = "aeiouy";
-    //     ArrayList<Character> list1 = new ArrayList<>();
-    //     ArrayList<Character> list2 = new ArrayList<>();
-    //     ArrayList<String> list3 = new ArrayList<>();
-    //}
-
+    public static ArrayList<String> sameVowelGroup(String [] arr) {
+        String lets = "aeiouy";
+        ArrayList<Character> list1 = new ArrayList<>();
+        ArrayList<Character> list2 = new ArrayList<>();
+        ArrayList<String> list3 = new ArrayList<>();
+        for (int i = 0; i < arr[0].length(); i++) { //гласные в первом слове
+            if (lets.indexOf(arr[0].charAt(i)) != -1)
+                list1.add(arr[0].charAt(i));
+        }
+        Collections.sort(list1);
+        for (int i = 1; i < list1.size(); i++) { //одинаковые в первом слове
+            if ((list1.get(i)).equals(list1.get(i-1))) 
+                list1.remove(i-1);
+        } 
+        for (int i = 0; i < arr.length; i++) { //для каждого слова в исх. массиве
+            for (int j = 0; j < arr[i].length(); j++) { 
+                if (lets.indexOf(arr[i].charAt(j)) != -1) 
+                    list2.add(arr[i].charAt(j));
+                Collections.sort(list2);
+                for (int n = 1; n < list2.size(); n++) {
+                    if ((list2.get(n)).equals(list2.get(n-1))) 
+                        list2.remove(n-1);
+                }
+            }
+            if (list1.size() == list2.size()) {
+                for (int n = 0; n < list2.size(); n++) {
+                    if (list1.get(n) == list2.get(n)) 
+                        list3.add(arr[i]);
+                    else 
+                        break;
+                }
+            }
+            list2.clear();
+        }
+        for (int i = 1; i < list3.size(); i++) { //одинаковые слова
+            if ((list3.get(i)).equals(list3.get(i-1))) 
+                list3.remove(i-1);
+        }
+        return list3;
+    }
+    // 6/10
+    public static boolean validateCard(long n) {
+        int s = 0;
+        ArrayList<Character> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        String st = String.valueOf(n).substring(0, String.valueOf(n).length()-1);
+        int k = Integer.valueOf(String.valueOf(n).substring(String.valueOf(n).length()-1));
+        if (st.length() < 13 || st.length() > 18)
+            return false;
+        for (int i = st.length() - 1; i >= 0; i--) 
+            list1.add(st.charAt(i));
+        for (int i = 0; i < list1.size(); i++) {
+            if (i % 2 == 0) 
+                list2.add(Character.getNumericValue(list1.get(i)) * 2);
+            else 
+                list2.add(Character.getNumericValue(list1.get(i)));
+        }
+        for (int l = 0; l < list1.size(); l++) {
+            if (list2.get(l) > 9) 
+                list2.set(l, list2.get(l) / 10 + list2.get(l) % 10);
+            s += list2.get(l);
+        }
+        int t = 10 - Integer.valueOf(String.valueOf(s).substring(String.valueOf(s).length()-1));
+        return (k == t);
+    }
     //7/10
     public static String numToEng(int n) {
         String s = new String();
@@ -134,11 +224,11 @@ public class Task5 {
             s = nums.get(n);
         if (n > 20 && n <= 100) {
             a = n % 10;
-            b = n;
+            b = (n / 10) * 10;
             if (a != 0) 
-                s = nums.get(n) + " " + nums.get(a);
+                s = nums.get(b) + " " + nums.get(a);
             else
-                s = nums.get(n);
+                s = nums.get(b);
         }
         if (n > 100 && n < 1000) {
             a = n % 10;
@@ -229,5 +319,47 @@ public class Task5 {
         byte [] digest = m.digest(input);
         String sha256 = DatatypeConverter.printHexBinary(digest).toLowerCase();
         return sha256;
+    }
+    // 9/10
+    public static String correctTitle(String s) {
+        String str = new String();
+        String [] arr = s.toLowerCase().split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("and") || arr[i].equals("the") || arr[i].equals("of") || arr[i].equals("in")) 
+                str += arr[i] + " ";
+            else str += arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1) + " ";
+        }
+        return str;
+    }
+    // 10/10
+    public static String haxLattice(int n) {
+        int num = 1;
+        int i = 1;
+        String res = "";
+        while (num < n) {
+            i++;
+            num = 3 * i * (i - 1) + 1; //является ли шестиугольником
+        }
+        int k = i; //сколько в первой и последней строках
+        System.out.println(i);
+        if (n != num)
+            res = "invalid";
+        while (k < i * 2 - 1) { //до середины
+            for (int a = 0; a < i * 2 - 1 - k; a++)
+                res += " "; //колво пробелов = отдаленность от середины
+            for (int b = 0; b < k; b++)
+                res += "o "; //когда напечатали все пробелы
+            res += "\n";
+            k++; //на следующей строке +1 "о"
+        }
+        while (k >= i) { //середина и после
+            for (int a = 0; a < i * 2 - 1 - k; a++)
+                res += " ";
+            for (int b = k; b > 0; b--)
+                res += "o ";
+            res += "\n";
+            k--; //на следующей строке -1 "о"
+        }
+        return res;
     }
 }
